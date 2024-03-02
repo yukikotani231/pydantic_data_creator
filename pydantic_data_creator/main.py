462bec92b730ada0afbc9142735774bf1c72cf97
yukikotani231 <kotani-yuki231@g.ecc.u-tokyo.ac.jp>
2024-03-02 23:44:30 +0900

    modified main.py

--- pydantic_data_creator/main.py
+++ pydantic_data_creator/main.py
@@ -7,21 +7,12 @@ import streamlit as st
 import streamlit_pydantic as sp
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "module_path", help="The path to the module that includes the pydantic models"
-    )
-    parser.add_argument("model_name", help="The name of the pydantic model to use for the form")
-    args = parser.parse_args()
-
-    module = import_module(args.module_path)
-    model = getattr(module, args.model_name)
+def main(module_path: str, model_name: str):
+    module = import_module(module_path)
+    model = getattr(module, model_name)
 
     if not issubclass(model, BaseModel) and not is_dataclass(model):
-        st.error(
-            f"{args.module_path}.{args.model_name} is not a valid pydantic model or dataclass."
-        )
+        st.error(f"{module_path}.{model_name} is not a valid pydantic model or dataclass.")
         st.stop()
 
     data = sp.pydantic_form(key="my_form", model=model)
@@ -30,4 +21,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "module_path", help="The path to the module that includes the pydantic models"
+    )
+    parser.add_argument("model_name", help="The name of the pydantic model to use for the form")
+    args = parser.parse_args()
+
+    main(args.module_path, args.model_name)
